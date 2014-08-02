680b2880aca087f82cfb3ce86fba1fa8281270ed
psparago <peter.sparago@gmail.com>
2014-08-02 09:30:43 -0400

    bulk insert

--- src/org/sparago/udacity/sunshineapp/data/WeatherProvider.java
+++ src/org/sparago/udacity/sunshineapp/data/WeatherProvider.java
@@ -7,6 +7,8 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
@@ -134,18 +136,114 @@ public class WeatherProvider extends ContentProvider {
 
 	@Override
 	public Uri insert(Uri uri, ContentValues contentValues) {
-		return null;
+		final SQLiteDatabase db = dbHelper.getWritableDatabase();
+		final int match = sUriMatcher.match(uri);
+		Uri returnUri = null;
+		
+		switch (match) {
+		// "weather"
+		case WEATHER: {
+			long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, contentValues);
+			if (_id > 0)
+				returnUri = WeatherContract.WeatherEntry.buildWeatherUri(_id);
+			else
+				throw new SQLException("failed to insert row into " + uri);
+			break;
+		}
+		case LOCATION: {
+			long _id = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, contentValues);
+			if (_id > 0)
+				returnUri = WeatherContract.LocationEntry.buildLocationUri(_id);
+			else
+				throw new SQLException("failed to insert row into " + uri);
+			break;
+		}
+		default:
+			throw new UnsupportedOperationException("Unknown uri" + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return returnUri;
 	}
 
 	@Override
-	public int delete(Uri uri, String s, String[] strings) {
-		return 0;
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		final SQLiteDatabase db = dbHelper.getWritableDatabase();
+		final int match = sUriMatcher.match(uri);
+		int rowsAffected = 0;
+		
+		switch (match) {
+		// "weather"
+		case WEATHER: {
+			rowsAffected = db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection, selectionArgs);
+			break;
+		}
+		case LOCATION: {
+			rowsAffected = db.delete(WeatherContract.LocationEntry.TABLE_NAME, selection, selectionArgs);
+			break;
+		}
+		default:
+			throw new UnsupportedOperationException("Unknown uri" + uri);
+		}
+		// null selection deletes all rows
+		if (selection == null || rowsAffected != 0) {
+			getContext().getContentResolver().notifyChange(uri, null);
+		}
+		return rowsAffected;
 	}
 
 	@Override
-	public int update(Uri uri, ContentValues contentValues, String s,
-			String[] strings) {
-		return 0;
+	public int update(Uri uri, ContentValues contentValues, String selection,
+			String[] selectionArgs) {
+		final SQLiteDatabase db = dbHelper.getWritableDatabase();
+		final int match = sUriMatcher.match(uri);
+		int rowsAffected = 0;
+		
+		switch (match) {
+		// "weather"
+		case WEATHER: {
+			rowsAffected = db.update(WeatherContract.WeatherEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+			break;
+		}
+		case LOCATION: {
+			rowsAffected = db.update(WeatherContract.LocationEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+			break;
+		}
+		default:
+			throw new UnsupportedOperationException("Unknown uri" + uri);
+		}
+		if (rowsAffected != 0) {
+			getContext().getContentResolver().notifyChange(uri, null);
+		}
+		return rowsAffected;
+	}
+	
+	@Override
+	public int bulkInsert(Uri uri, ContentValues[] values) {
+		final SQLiteDatabase db = dbHelper.getWritableDatabase();
+		final int match = sUriMatcher.match(uri);
+		switch(match) {
+		case WEATHER: {
+			db.beginTransaction();
+			int returnCount = 0;
+			try {
+				for(ContentValues value : values) {
+					long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, value);
+					if (_id != -1) {
+						++returnCount;
+					}
+				}
+				db.setTransactionSuccessful();
+			} finally {
+				db.endTransaction();
+			}
+			if (returnCount > 0) {
+				getContext().getContentResolver().notifyChange(uri, null);
+			}
+			return returnCount;
+		}
+		default:
+			return super.bulkInsert(uri, values);
+		}
 	}
 
 	private static UriMatcher buildUriMatcher() {
