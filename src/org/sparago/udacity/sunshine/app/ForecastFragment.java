26fed77ac43e5331dae35dca07255e84287aa55d
psparago <peter.sparago@gmail.com>
2014-08-03 19:15:19 -0400

    stuff

--- src/org/sparago/udacity/sunshine/app/ForecastFragment.java
+++ src/org/sparago/udacity/sunshine/app/ForecastFragment.java
@@ -15,7 +15,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
@@ -29,7 +29,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ForecastFragment extends Fragment implements LoaderCallbacks<Cursor> {
+public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 	private static final int FORECAST_LOADER = 0;
 	
 	// Subset of columns used for the forecast view.
